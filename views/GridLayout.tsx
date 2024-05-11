1e383a52c555d54d568e44e9857894a5b71db69e
Naibel <dorian.belhaj@gmail.com>
2024-05-12 01:25:15 +0200

    fix: move GridLayout to views folder

--- views/GridLayout.tsx
+++ views/GridLayout.tsx
@@ -1,17 +1,58 @@
 import { useMemo, useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 
-import BarChart from "../BarChart/BarChart";
-import LineChart from "../LineChart/LineChart";
-import PieChart from "../PieChart/PieChart";
-import { Widget } from "../Widget/Widget";
-
+import BarChart from "@/components/BarChart/BarChart";
+import LineChart from "@/components/LineChart/LineChart";
+import Menu from "@/components/Menu/Menu";
+import PieChart from "@/components/PieChart/PieChart";
+import Table from "@/components/Table/Table";
+import { Widget } from "@/components/Widget/Widget";
 import { useAlertStore } from "@/hooks";
-import { Cols, Item, LayoutType } from "@/types/types";
+import { columnsData } from "@/mocks/columnsData";
+import mockData from "@/mocks/MOCK_DATA.json";
+import { ChartType, Cols, Item, LayoutType } from "@/types/types";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-type GridLayoutType = {
+const getComponent = (chart: ChartType) => {
+  switch (chart) {
+    case "pie":
+      return <PieChart />;
+    case "bar":
+      return <BarChart />;
+    case "line":
+      return <LineChart />;
+    case "table":
+      return <Table tableData={mockData} columnsData={columnsData} />;
+    default:
+      return <Table tableData={mockData} columnsData={columnsData} />;
+  }
+};
+
+const chartData = {
+  pie: {
+    title: "Diagramme en camenbert",
+    w: 2,
+    h: 3,
+  },
+  bar: {
+    title: "Diagramme en barres",
+    w: 3,
+    h: 3,
+  },
+  line: {
+    title: "Diagramme en lignes",
+    w: 3,
+    h: 3,
+  },
+  table: {
+    title: "Tableau",
+    w: 6,
+    h: 4,
+  },
+};
+
+type GridLayoutProps = {
   className?: string;
   cols?: Cols;
   rowHeight?: number;
@@ -23,39 +64,30 @@ const GridLayout = ({
   cols = { lg: 8, md: 6, sm: 6, xs: 4, xxs: 2 },
   rowHeight = 100,
   onLayoutChange,
-}: GridLayoutType) => {
+}: GridLayoutProps) => {
   const { setAlertMessage } = useAlertStore();
 
-  const [items, setItems] = useState<Item[]>(
-    [0, 1, 2, 3, 4].map(function (i) {
-      return {
-        i: i.toString(),
-        x: i * 2,
-        y: 0,
-        w: 2,
-        h: 2,
-      };
-    })
-  );
-
+  const [items, setItems] = useState<Item[]>([]);
   const [newCounter, setNewCounter] = useState(items.length);
   // const [breakpoint, setBreakpoint] = useState(0);
   const [currentCols, setCurrentCols] = useState(0);
   // const [layout, setLayout] = useState(0);
 
   const createElement = (item: Item) => {
-    const i = item.add ? "+" : item.i;
-
     return (
-      <div key={i} data-grid={item}>
-        <Widget index={i} onRemove={onRemoveItem}>
-          <PieChart />
+      <div key={item.i} data-grid={item}>
+        <Widget
+          title={chartData[item.type].title}
+          index={item.i}
+          onRemove={onRemoveItem}
+        >
+          {getComponent(item.type)}
         </Widget>
       </div>
     );
   };
 
-  const onAddItem = () => {
+  const onAddItem = (chart: ChartType) => {
     /*eslint no-console: 0*/
     console.log("adding", "n" + newCounter);
 
@@ -66,8 +98,9 @@ const GridLayout = ({
           i: "n" + newCounter,
           x: (prevState.length * 2) % (currentCols || 12),
           y: Infinity, // puts it at the bottom
-          w: 2,
-          h: 2,
+          w: chartData[chart].w,
+          h: chartData[chart].h,
+          type: chart,
         };
         return [...prevState, newEntry];
       });
@@ -83,7 +116,8 @@ const GridLayout = ({
 
   const onRemoveItem = (i: string) => {
     console.log("removing", i);
-    setItems((prevState) => prevState.filter((item) => item.i !== i));
+    setItems((prevItems) => prevItems.filter((item) => item.i !== i));
+    setNewCounter((counter) => counter - 1);
   };
 
   // We're using the cols coming back from this to calculate where to add new items.
@@ -97,7 +131,7 @@ const GridLayout = ({
     // setLayout(layout);
   };
 
-  const children = useMemo(
+  const grids = useMemo(
     () => items.map((item) => createElement(item)),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [newCounter]
@@ -105,7 +139,7 @@ const GridLayout = ({
 
   return (
     <div>
-      <button onClick={onAddItem}>Add Item</button>
+      <Menu onAddItem={onAddItem} />
       <ResponsiveGridLayout
         onLayoutChange={onLayoutChangeHandler}
         onBreakpointChange={onBreakpointChange}
@@ -113,7 +147,7 @@ const GridLayout = ({
         cols={cols}
         rowHeight={rowHeight}
       >
-        {children}
+        {grids}
       </ResponsiveGridLayout>
     </div>
   );
