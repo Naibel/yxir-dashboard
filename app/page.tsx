53e179e05ab808e3ebd1602626614b079da85e21
Naibel <dorian.belhaj@gmail.com>
2024-05-12 01:22:57 +0200

    style: fix page title style

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 
-import GridLayout from "@/components/GridLayout/GridLayout";
 import withProviders from "@/providers/withProviders.hoc";
+import GridLayout from "@/views/GridLayout";
 
 const Home = () => {
   return (
     <main className="p-10 h-screen">
-      <div className="">
-        <h1>Yxir Dashboard</h1>
+      <div>
+        <h1 className="text-2xl  font-bold">Yxir Dashboard</h1>
       </div>
       <div>
         <GridLayout />
