49e46760771bc05e765ddb42a1b39607a4f4a648
Naibel <dorian.belhaj@gmail.com>
2024-05-13 21:42:11 +0200

    style: drag only on header click, several style changes

--- components/Widget/Widget.tsx
+++ components/Widget/Widget.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { IoClose } from "react-icons/io5";
+import { Tooltip } from "react-tooltip";
 
 type WidgetProps = {
   title: string;
@@ -8,23 +9,46 @@ type WidgetProps = {
   children: ReactNode;
 };
 
-export const Widget = ({ title, index, onRemove, children }: WidgetProps) => (
-  <div className="flex flex-col box-sizing bg-white p-5 h-full shadow-indigo-100 rounded-xl shadow-lg hover:shadow-xl duration-300 ">
-    <div className="flex allowGrab cursor-grab">
-      <span className="text flex-1 text-lg font-bold">{title}</span>
-      <button
-        className="cancelGrab remove"
-        style={{
-          cursor: "pointer",
-          zIndex: 50,
-        }}
-        onClick={() => {
-          onRemove(index);
-        }}
+const Widget = ({ title, index, onRemove, children }: WidgetProps) => {
+  const [isHeaderOnHover, setIsHeaderOnHover] = useState<boolean>(false);
+
+  return (
+    <div
+      className={`flex flex-col box-sizing bg-white h-full shadow-indigo-200 rounded-xl ${
+        isHeaderOnHover ? "shadow-lg opacity-50" : ""
+      } duration-300`}
+    >
+      <div
+        onMouseEnter={() => setIsHeaderOnHover(true)}
+        onMouseLeave={() => setIsHeaderOnHover(false)}
+        className="flex allowGrab cursor-grab px-5 pt-5"
       >
-        <IoClose size={20} className="opacity-50 hover:opacity-100" />
-      </button>
+        <span className="text flex-1 text-lg font-bold text-indigo-950">
+          {title}
+        </span>
+      </div>
+      <div className="absolute right-5 top-5">
+        <a
+          data-tooltip-id="close-widget"
+          data-tooltip-content="Supprimer ce widget"
+        >
+          <button
+            className="cursor-pointer z-50 text-indigo-600 cancelGrab bg-indigo-50 rounded-full p-1 shadow-indigo-100 hover:bg-indigo-200"
+            onClick={() => {
+              onRemove(index);
+            }}
+          >
+            <IoClose size={20} className="opacity-100" />
+          </button>
+          <Tooltip
+            style={{ backgroundColor: "#6366f1", color: "#fff" }}
+            id="close-widget"
+          />
+        </a>
+      </div>
+      <div className="h-full px-5 pb-5 overflow-auto">{children}</div>
     </div>
-    <div className="h-full overflow-auto ">{children}</div>
-  </div>
-);
+  );
+};
+
+export default Widget;
