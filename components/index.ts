438bd16950a5e4a88cc75ad38868d33748bab893
Naibel <dorian.belhaj@gmail.com>
2024-05-13 21:39:03 +0200

    fix: add index page on components folder

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import { default as Alert } from "./Alert/Alert";
+import { default as Chart } from "./Chart/Chart";
+import { default as LayoutPlaceholder } from "./LayoutPlacehoder/LayoutPlaceholder";
+import { default as Menu } from "./Menu/Menu";
+import { default as Table } from "./Table/Table";
+import { default as Widget } from "./Widget/Widget";
+
+export { Alert };
+export { Chart };
+export { LayoutPlaceholder };
+export { Menu };
+export { Table };
+export { Widget };
