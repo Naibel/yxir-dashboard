05b98f53d7b5682d3d0a965dc6e1a1f1d3319c2f
Naibel <dorian.belhaj@gmail.com>
2024-05-13 16:46:58 +0200

    fix: menu icon color fix

--- components/Menu/ChartButton.tsx
+++ components/Menu/ChartButton.tsx
@@ -17,7 +17,7 @@ const ChartButton = ({
   <div onClick={onClick}>
     <a data-tooltip-id={tooltipId} data-tooltip-content={tooltipText}>
       <button
-        className="shadow-indigo-200 text-black shadow-lg z-20 p-4 bg-white hover:bg-neutral-600 hover:text-white active:bg-neutral-700 duration-300 rounded-full text-white"
+        className="shadow-indigo-200 text-neutral-600 shadow-lg z-20 p-4 bg-white hover:bg-neutral-600 hover:text-white active:bg-neutral-700 duration-300 rounded-full text-white"
         onClick={() => {}}
       >
         {children}
